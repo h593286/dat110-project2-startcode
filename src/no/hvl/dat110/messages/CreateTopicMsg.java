6fae63efadc826ff7b74d103da901b734d46415a
eilert <eilertskram@gmail.com>
2022-03-02 12:56:34 +0100

    First commit

--- src/no/hvl/dat110/messages/CreateTopicMsg.java
+++ src/no/hvl/dat110/messages/CreateTopicMsg.java
@@ -1,12 +1,37 @@
 package no.hvl.dat110.messages;
 
 public class CreateTopicMsg extends Message {
-	
+	private String topic;
 	// message sent from client to create topic on the broker
 	
 	// TODO: 
 	// Implement object variables - a topic is required
 	
 	// Constructor, get/set-methods, and toString method
-    // as described in the project text	
+    // as described in the project text
+
+    public CreateTopicMsg (String user, String topic) {
+        super(MessageType.CREATETOPIC, user);
+        this.topic = topic;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getUser(){
+        return super.getUser();
+    }
+    public MessageType getType() {
+        return super.getType();
+    }
+
+    @Override
+    public String toString() {
+        return "Message [type=" + getType() + ", user=" + getUser() + ", topic=" + topic +"]";
+    };
 }
